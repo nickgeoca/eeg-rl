aede7c5aefde4591da5490c5412c3a3030955222
Nick Geoca <nickgeoca@gmail.com>
2026-04-16 10:44:33 +0800

    nix: add CUDA support to dev shell

--- flake.nix
+++ flake.nix
@@ -9,16 +9,28 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      python = pkgs.python3.withPackages (ps: [
-        ps.numpy
-        ps.gymnasium
-        ps.plotext
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      cuda = pkgs.cudaPackages;
+      libs = pkgs.lib.makeLibraryPath (with pkgs; [
+        stdenv.cc.cc zlib zstd curl openssl bzip2 libxml2 xz systemd
+        cudaPackages.cudatoolkit
+        cudaPackages.cuda_cudart
       ]);
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ python ];
+        buildInputs = with pkgs; [
+          python3 uv
+          cudaPackages.cudatoolkit
+          cudaPackages.cuda_cudart
+        ];
+
+        shellHook = ''
+          export UV_PYTHON_DOWNLOADS=never
+          export LD_LIBRARY_PATH=${libs}:/run/opengl-driver/lib:$LD_LIBRARY_PATH
+          uv sync --quiet
+          source .venv/bin/activate
+        '';
       };
     };
 }
